2e31be417e78e1a3786e3169d1c1436c3f825fe4
jstanley9 <jim.stanley9@gmail.com>
2025-09-16 14:39:08 -0400

    Validating the revised block structure, more to come

--- src/ravrf/blockDescriptor.py
+++ src/ravrf/blockDescriptor.py
@@ -23,40 +23,78 @@ class HeadBlock:
     # is at the location being read
     __STRUCT_MASK = ">BIIIH"
 
-    def __init__(self, block_type: BlockType, data_length: int, record_size: int, padding: int, checksum: int):
+    def __init__(self, block_type: BlockType, record_size: int, prev_or_data: int, 
+                 next_or_padding: int, checksum: int):
         self.block_type = block_type
         self.record_size = record_size
-        if self.isAvailable():
-            self.next_available = data_length
-            self.prev_available = padding
-        else:
-            self.data_length = data_length
-            self.padding = padding
+        self.__next_or_open_size = next_or_padding
+        self.__prev_or_data_size = prev_or_data
 
         if checksum != 0:
             calc_checksum = self.__getChecksum()
             if checksum != calc_checksum:
                raise ValueError(f"HeadBlock checksum ({checksum}) does not match calculated checksum ({calc_checksum})")
 
+    
+    @classmethod
+    def initAvailable(cls, record_size: int, prev_available: int, next_avail: int, checksum: int):
+        return cls(BlockType.AVAILABLE, record_size, prev_available, next_avail, checksum)
+    
+
+    @classmethod
+    def initMeta(cls, record_size: int, data_size: int, open_size: int, checksum: int):
+        return cls(BlockType.META_BLOCK, record_size, data_size, open_size, checksum)
+    
+
+    @classmethod
+    def initData(cls, record_size: int, data_size: int, open_size: int, checksum: int):
+        return cls(BlockType.DATA_BLOCK, record_size, data_size, open_size, checksum)
+    
+
     @property
     def next_available(self):
-        return self.data_length
+        return self.__next_or_open_size
     
+
     @property
     def prev_available(self):
-        return self.padding
+        return self.__prev_or_data_size
     
+
     @next_available.setter
     def next_available(self, next_available):
-        self.data_length = next_available
+        self.__next_or_open_size = next_available
+
 
     @prev_available.setter
     def prev_available(self, prev_available):
-        self.padding = prev_available
+        self.__prev_or_data_size = prev_available
+
+
+    @property
+    def data_size(self):
+        return self.__prev_or_data_size
+
+
+    @property
+    def open_size(self):
+        return self.__next_or_open_size
+
+
+    @data_size.setter
+    def data_size(self, data_size):
+        self.__prev_or_data_size = data_size
+
+
+    @open_size.setter
+    def open_size(self, open_size):
+        self.__prev_or_data_size = open_size
+
 
     def isAvailable(self):
         return self.block_type == BlockType.AVAILABLE
 
+    
     def __calcRecordSize(self, data_length: int, record_size: int = 0):
         minRequiredLength = CalcMinBlockSize() + data_length
         if record_size < minRequiredLength:
@@ -66,36 +104,23 @@ class HeadBlock:
     
 
     def __getChecksum(self):
-        return calc_16bit_checksum([self.block_type.value, self.data_length, self.record_size, self.padding])
+        return calc_16bit_checksum([self.block_type.value, self.record_size, 
+                                    self.__prev_or_data_size, self.__next_or_open_size])
 
 
     def encode(self) -> bytearray:
-        match self.block_type:
-            case BlockType.AVAILABLE:
-                # Format: B I I I H (1 byte, 4 bytes, 4 bytes, 4 bytes, 2 bytes)
-                return bytearray(struct.pack(self.__STRUCT_MASK,    
-                                            self.block_type, self.record_size, self.next_avail, self.prev_avail, 
-                                            self.__getChecksum()))
-            case BlockType.META_BLOCK | BlockType.DATA_BLOCK:                 
-                # Format: B I I I H (1 byte, 4 bytes, 4 bytes, 4 bytes, 2 bytes)
-                return bytearray(struct.pack(self.__STRUCT_MASK,    
-                                            self.block_type, self.record_size, self.data_length, self.padding, 
-                                            self.__getChecksum()))
-            case _:
-                raise ValueError(f"Invalid block type: {self.block_type}:{self.block_type.value}")
+        # Format: B I I I H (1 byte, 4 bytes, 4 bytes, 4 bytes, 2 bytes)
+        return bytearray(struct.pack(self.__STRUCT_MASK,    
+                                     self.block_type, self.record_size, 
+                                     self.__prev_or_data_size, self.__next_or_open_size, 
+                                     self.__getChecksum()))
 
     @classmethod
     def decode(cls, data: bytearray):
         block_type = BlockType(data[0])
-        match block_type:
-            case BlockType.AVAILABLE:
-                _, record_size, next_available, prev_available, checksum = struct.unpack(cls.__STRUCT_MASK, data)
-                return cls(block_type, next_available, record_size, prev_available, checksum)
-            case BlockType.META_BLOCK | BlockType.DATA_BLOCK:
-                _, record_size, data_length, padding, checksum = struct.unpack(cls.__STRUCT_MASK, data)
-                return cls(block_type, data_length, record_size, padding, checksum)
-            case _:
-                raise ValueError(f"Invalid block type: {block_type}:{block_type.value}")
+        _, record_size, prev_available, next_available, checksum = struct.unpack(cls.__STRUCT_MASK, data)
+        return cls(block_type, record_size, prev_available, next_available, checksum)
+
 
     @classmethod
     def getStorageSize(cls):
@@ -124,4 +149,4 @@ class EndBlock:
     
 
 def CalcMinBlockSize() -> int:
-    return HeadBlock.getStorageSize() + EndBlock.getStorageSize()    
+    return HeadBlock.getStorageSize() + EndBlock.getStorageSize()
--- tests/ravrf/test_blockDescriptor.py
+++ tests/ravrf/test_blockDescriptor.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import pytest
 import struct
 import sys
 
@@ -8,182 +9,187 @@ print(f"Path = {srcPath}")
 import blockDescriptor
 
 def test_getHeadStorageSize():
-    assert blockDescriptor.HeadBlock.getStorageSize() == 7
+    assert blockDescriptor.HeadBlock.getStorageSize() == 15
 
 def test_emptyHeadBlock():
-    head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAILABLE, 0x11223344, 234)
-    assert head.encode() == bytearray(b'\x40\x11\x22\x33\x44\x00\xea')
+    head = blockDescriptor.HeadBlock.initAvailable(0, 0, 0, 65)
+    assert head.encode() == bytearray(b'\x41\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x41')
 
-def test_avail_1byte():
-    head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAIL_01_BYTE, 1, 0)
-    assert head.encode() == bytearray(b'\x01')
+def test_avail_1byte_new():
+    head = blockDescriptor.HeadBlock.initAvailable(1, 0, 0, 66)
+    assert head.encode() == bytearray(b'\x41\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x42')
 
-def test_avail_1byte_available():
-    head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAILABLE, 1, 0)
-    assert head.encode() == bytearray(b'\x01')    
+def test_avail_1byte_linked():
+    head = blockDescriptor.HeadBlock.initAvailable(1, 170, 255, 491)
+    assert head.encode() == bytearray(b'\x41\x00\x00\x00\x01\x00\x00\x00\xaa\x00\x00\x00\xff\x01\xeb')
 
 def test_avail_1byte_decode():
-    head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAIL_01_BYTE, 1, 0)
+    head = blockDescriptor.HeadBlock.initAvailable(1, 170, 255, 491)
     data = head.encode()
     newHead = blockDescriptor.HeadBlock.decode(data)
     assert newHead.encode() == data
 
-def test_avail_2byte():
-    head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAIL_02_BYTE, 2, 0)
-    assert head.encode() == bytearray(b'\x02\x02')
+def test_invalid_block_type():
+    with pytest.raises(ValueError) as excinfo:
+        head = blockDescriptor.HeadBlock(blockDescriptor.BlockType(99), 1, 0, 0, 0)
+    assert str(excinfo.value) == "99 is not a valid BlockType"    
 
-def test_avail_2byte_available():
-    head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAILABLE, 2, 0)
-    assert head.encode() == bytearray(b'\x02\x02')    
+# def test_avail_2byte():
+#     head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAIL_02_BYTE, 2, 0)
+#     assert head.encode() == bytearray(b'\x02\x02')
 
-def test_avail_2byte_decode():
-    head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAIL_02_BYTE, 2, 0)
-    data = head.encode()
-    newHead = blockDescriptor.HeadBlock.decode(data)
-    assert newHead.encode() == data
+# def test_avail_2byte_available():
+#     head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAILABLE, 2, 0)
+#     assert head.encode() == bytearray(b'\x02\x02')    
 
-def test_avail_3byte():
-    head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAIL_03_BYTE, 3, 0)
-    print(f"HeadBlock: {head}")
-    assert head.encode() == bytearray(b'\x03\x03\x03')
+# def test_avail_2byte_decode():
+#     head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAIL_02_BYTE, 2, 0)
+#     data = head.encode()
+#     newHead = blockDescriptor.HeadBlock.decode(data)
+#     assert newHead.encode() == data
 
-def test_avail_3byte_available():
-    head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAILABLE, 3, 0)
-    print(f"HeadBlock: {head}")
-    assert head.encode() == bytearray(b'\x03\x03\x03')    
+# def test_avail_3byte():
+#     head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAIL_03_BYTE, 3, 0)
+#     print(f"HeadBlock: {head}")
+#     assert head.encode() == bytearray(b'\x03\x03\x03')
 
-def test_avail_3byte_decode():
-    head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAIL_03_BYTE, 3, 0)
-    data = head.encode()
-    newHead = blockDescriptor.HeadBlock.decode(data)
-    assert newHead.encode() == data
+# def test_avail_3byte_available():
+#     head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAILABLE, 3, 0)
+#     print(f"HeadBlock: {head}")
+#     assert head.encode() == bytearray(b'\x03\x03\x03')    
 
-def test_avail_6byte():
-    head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAIL_06_BYTE, 6, 0)
-    print(f"HeadBlock: {head}")
-    assert head.encode() == bytearray(b'\x06\x06\x06\x06\x06\x06')
+# def test_avail_3byte_decode():
+#     head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAIL_03_BYTE, 3, 0)
+#     data = head.encode()
+#     newHead = blockDescriptor.HeadBlock.decode(data)
+#     assert newHead.encode() == data
 
-def test_avail_6byte_available():
-    head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAILABLE, 6, 0)
-    print(f"HeadBlock: {head}")
-    assert head.encode() == bytearray(b'\x06\x06\x06\x06\x06\x06')
+# def test_avail_6byte():
+#     head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAIL_06_BYTE, 6, 0)
+#     print(f"HeadBlock: {head}")
+#     assert head.encode() == bytearray(b'\x06\x06\x06\x06\x06\x06')
 
-def test_avail_6byte_decode():
-    head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAIL_06_BYTE, 6, 0)
-    data = head.encode()
-    newHead = blockDescriptor.HeadBlock.decode(data)
-    assert newHead.encode() == data
+# def test_avail_6byte_available():
+#     head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAILABLE, 6, 0)
+#     print(f"HeadBlock: {head}")
+#     assert head.encode() == bytearray(b'\x06\x06\x06\x06\x06\x06')
 
-def test_avail_7byte():
-    head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAIL_07_BYTE, 7, 0)
-    print(f"HeadBlock: {head}")
-    assert head.encode() == bytearray(b'\x07\x07\x07\x07\x07\x07\x07')
+# def test_avail_6byte_decode():
+#     head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAIL_06_BYTE, 6, 0)
+#     data = head.encode()
+#     newHead = blockDescriptor.HeadBlock.decode(data)
+#     assert newHead.encode() == data
 
-def test_avail_7byte_available():
-    head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAILABLE, 7, 0)
-    print(f"HeadBlock: {head}")
-    assert head.encode() == bytearray(b'\x07\x07\x07\x07\x07\x07\x07')
+# def test_avail_7byte():
+#     head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAIL_07_BYTE, 7, 0)
+#     print(f"HeadBlock: {head}")
+#     assert head.encode() == bytearray(b'\x07\x07\x07\x07\x07\x07\x07')
 
-def test_avail_7byte_decode():
-    head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAIL_07_BYTE, 7, 0)
-    data = head.encode()
-    newHead = blockDescriptor.HeadBlock.decode(data)
-    assert newHead.encode() == data
+# def test_avail_7byte_available():
+#     head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAILABLE, 7, 0)
+#     print(f"HeadBlock: {head}")
+#     assert head.encode() == bytearray(b'\x07\x07\x07\x07\x07\x07\x07')
 
-def test_avail_8byte():
-    head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAIL_08_BYTE, 8, 0)
-    assert head.encode() == bytearray(b'\x08\x08\x08\x08\x08\x08\x08\x08')
+# def test_avail_7byte_decode():
+#     head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAIL_07_BYTE, 7, 0)
+#     data = head.encode()
+#     newHead = blockDescriptor.HeadBlock.decode(data)
+#     assert newHead.encode() == data
 
-def test_avail_8byte_available():
-    head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAILABLE, 8, 0)
-    assert head.encode() == bytearray(b'\x08\x08\x08\x08\x08\x08\x08\x08')
+# def test_avail_8byte():
+#     head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAIL_08_BYTE, 8, 0)
+#     assert head.encode() == bytearray(b'\x08\x08\x08\x08\x08\x08\x08\x08')
 
-def test_avail_8byte_decode():
-    head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAIL_08_BYTE, 8, 0)
-    data = head.encode()
-    newHead = blockDescriptor.HeadBlock.decode(data)
-    assert newHead.encode() == data
+# def test_avail_8byte_available():
+#     head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAILABLE, 8, 0)
+#     assert head.encode() == bytearray(b'\x08\x08\x08\x08\x08\x08\x08\x08')
 
-def test_avail_12byte():
-    head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAIL_12_BYTE, 12, 0)
-    print(f"HeadBlock: {head}")
-    assert head.encode() == bytearray(b'\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c')
+# def test_avail_8byte_decode():
+#     head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAIL_08_BYTE, 8, 0)
+#     data = head.encode()
+#     newHead = blockDescriptor.HeadBlock.decode(data)
+#     assert newHead.encode() == data
 
-def test_avail_12byte_available():
-    head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAILABLE, 12, 0)
-    print(f"HeadBlock: {head}")
-    assert head.encode() == bytearray(b'\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c')
+# def test_avail_12byte():
+#     head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAIL_12_BYTE, 12, 0)
+#     print(f"HeadBlock: {head}")
+#     assert head.encode() == bytearray(b'\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c')
 
-def test_avail_12byte_decode():
-    head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAIL_12_BYTE, 12, 0)
-    data = head.encode()
-    newHead = blockDescriptor.HeadBlock.decode(data)
-    assert newHead.encode() == data
+# def test_avail_12byte_available():
+#     head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAILABLE, 12, 0)
+#     print(f"HeadBlock: {head}")
+#     assert head.encode() == bytearray(b'\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c')
 
-def test_avail_13byte_available():
-    head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAILABLE, 13, 77)
-    print(f"HeadBlock: {head}")
-    assert head.encode() == bytearray(b'\x40\x00\x00\x00\x0d\x00\x4d')
+# def test_avail_12byte_decode():
+#     head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAIL_12_BYTE, 12, 0)
+#     data = head.encode()
+#     newHead = blockDescriptor.HeadBlock.decode(data)
+#     assert newHead.encode() == data
 
-def test_avail_13byte_decode():
-    head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAILABLE, 13, 77)
-    data = head.encode()
-    newHead = blockDescriptor.HeadBlock.decode(data)
-    assert newHead.encode() == data
+# def test_avail_13byte_available():
+#     head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAILABLE, 13, 77)
+#     print(f"HeadBlock: {head}")
+#     assert head.encode() == bytearray(b'\x40\x00\x00\x00\x0d\x00\x4d')
 
-def test_meta_1byte():
-    head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.META_BLOCK, 1, 66)
-    assert head.encode() == bytearray(b'\x41\x00\x00\x00\x01\x00\x42')
+# def test_avail_13byte_decode():
+#     head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.AVAILABLE, 13, 77)
+#     data = head.encode()
+#     newHead = blockDescriptor.HeadBlock.decode(data)
+#     assert newHead.encode() == data
 
-def test_meta_1byte_decode():
-    head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.META_BLOCK, 1, 66)
-    data = head.encode()
-    newHead = blockDescriptor.HeadBlock.decode(data)
-    assert newHead.encode() == data
+# def test_meta_1byte():
+#     head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.META_BLOCK, 1, 66)
+#     assert head.encode() == bytearray(b'\x41\x00\x00\x00\x01\x00\x42')
 
-def test_meta_496byte():
-    head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.META_BLOCK, 496, 306)
-    assert head.encode() == bytearray(b'\x41\x00\x00\x01\xf0\x01\x32')
+# def test_meta_1byte_decode():
+#     head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.META_BLOCK, 1, 66)
+#     data = head.encode()
+#     newHead = blockDescriptor.HeadBlock.decode(data)
+#     assert newHead.encode() == data
 
-def test_meta_496byte_decode():
-    head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.META_BLOCK, 496, 306)
-    data = head.encode()
-    newHead = blockDescriptor.HeadBlock.decode(data)
-    assert newHead.encode() == data
+# def test_meta_496byte():
+#     head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.META_BLOCK, 496, 306)
+#     assert head.encode() == bytearray(b'\x41\x00\x00\x01\xf0\x01\x32')
+
+# def test_meta_496byte_decode():
+#     head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.META_BLOCK, 496, 306)
+#     data = head.encode()
+#     newHead = blockDescriptor.HeadBlock.decode(data)
+#     assert newHead.encode() == data
 
 def test_data_1byte():
-    head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.DATA_BLOCK, 1, 67)
-    assert head.encode() == bytearray(b'\x42\x00\x00\x00\x01\x00\x43')
+    head = blockDescriptor.HeadBlock.initData(1, 1, 0, 70)
+    assert head.encode() == bytearray(b'\x44\x00\x00\x00\x01\x00\x00\x00\x01\x00\x00\x00\x00\x00\x46')
 
 def test_data_1byte_decode():
-    head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.DATA_BLOCK, 1, 67)
+    head = blockDescriptor.HeadBlock.initData(1, 1, 0, 0)
     data = head.encode()
     newHead = blockDescriptor.HeadBlock.decode(data)
     assert newHead.encode() == data
 
-def test_data_496byte():
-    head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.DATA_BLOCK, 496, 307)
-    assert head.encode() == bytearray(b'\x42\x00\x00\x01\xf0\x01\x33')
+# def test_data_496byte():
+#     head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.DATA_BLOCK, 496, 307)
+#     assert head.encode() == bytearray(b'\x42\x00\x00\x01\xf0\x01\x33')
 
-def test_data_496byte_decode():
-    head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.DATA_BLOCK, 496, 307)
-    data = head.encode()
-    newHead = blockDescriptor.HeadBlock.decode(data)
-    assert newHead.encode() == data
+# def test_data_496byte_decode():
+#     head = blockDescriptor.HeadBlock(blockDescriptor.BlockType.DATA_BLOCK, 496, 307)
+#     data = head.encode()
+#     newHead = blockDescriptor.HeadBlock.decode(data)
+#     assert newHead.encode() == data
 
-def test_avail_13byte_end():
-    tail = blockDescriptor.EndBlock(13, blockDescriptor.BlockType.AVAILABLE)
-    assert tail.encode() == bytearray(b'\x00\x00\x00\x0d\x40')
+# def test_avail_13byte_end():
+#     tail = blockDescriptor.EndBlock(13, blockDescriptor.BlockType.AVAILABLE)
+#     assert tail.encode() == bytearray(b'\x00\x00\x00\x0d\x40')
 
-def test_avail_13byte_end_decode():
-    tail = blockDescriptor.EndBlock(13, blockDescriptor.BlockType.AVAILABLE)
-    data = tail.encode()
-    newTail = blockDescriptor.EndBlock.decode(data)
-    assert newTail.encode() == data
+# def test_avail_13byte_end_decode():
+#     tail = blockDescriptor.EndBlock(13, blockDescriptor.BlockType.AVAILABLE)
+#     data = tail.encode()
+#     newTail = blockDescriptor.EndBlock.decode(data)
+#     assert newTail.encode() == data
 
 def test_meta_1byte_end():
     tail = blockDescriptor.EndBlock(1, blockDescriptor.BlockType.META_BLOCK)
-    assert tail.encode() == bytearray(b'\x00\x00\x00\x01\x41')
+    assert tail.encode() == bytearray(b'\x00\x00\x00\x01\x4d')
 
 def test_meta_1byte_decode_end():
     tail = blockDescriptor.EndBlock(1, blockDescriptor.BlockType.META_BLOCK)
@@ -193,7 +199,7 @@ def test_meta_1byte_decode_end():
 
 def test_meta_496byte_end():
     tail = blockDescriptor.EndBlock(496, blockDescriptor.BlockType.META_BLOCK)
-    assert tail.encode() == bytearray(b'\x00\x00\x01\xf0\x41')
+    assert tail.encode() == bytearray(b'\x00\x00\x01\xf0\x4d')
 
 def test_meta_496byte_decode_end():
     tail = blockDescriptor.EndBlock(496, blockDescriptor.BlockType.META_BLOCK)
@@ -203,7 +209,7 @@ def test_meta_496byte_decode_end():
 
 def test_data_1byte_end():
     tail = blockDescriptor.EndBlock(1, blockDescriptor.BlockType.DATA_BLOCK)
-    assert tail.encode() == bytearray(b'\x00\x00\x00\x01\x42')
+    assert tail.encode() == bytearray(b'\x00\x00\x00\x01\x44')
 
 def test_data_1byte_decode_end():
     tail = blockDescriptor.EndBlock(1, blockDescriptor.BlockType.DATA_BLOCK)
@@ -213,7 +219,7 @@ def test_data_1byte_decode_end():
 
 def test_data_496byte_end():
     tail = blockDescriptor.EndBlock(496, blockDescriptor.BlockType.DATA_BLOCK)
-    assert tail.encode() == bytearray(b'\x00\x00\x01\xf0\x42')
+    assert tail.encode() == bytearray(b'\x00\x00\x01\xf0\x44')
 
 def test_data_496byte_decode_end():
     tail = blockDescriptor.EndBlock(496, blockDescriptor.BlockType.DATA_BLOCK)
@@ -221,10 +227,10 @@ def test_data_496byte_decode_end():
     newTail = blockDescriptor.EndBlock.decode(data)
     assert newTail.encode() == data
 
-def main():
-    print("Running tests for blockDescriptor")  
-    test_getHeadStorageSize()
-    test_emptyHeadBlock()
+# def main():
+#     print("Running tests for blockDescriptor")  
+#     test_getHeadStorageSize()
+#     test_emptyHeadBlock()
 
 if __name__ == "__main__":
     main()        
